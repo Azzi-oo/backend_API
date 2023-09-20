626bbd6c99f17036043546c14e36bc669a7955bf
Azat Yanberdin <azatdjon@yandex.ru>
2023-09-20 23:51:46 +0500

    add new cash fields

--- store/models.py
+++ store/models.py
@@ -8,7 +8,8 @@ class Book(models.Model):
     author_name = models.CharField(max_length=255)
     owner = models.ForeignKey(User, on_delete=models.SET_NULL,
                               null=True, related_name='my_books')
-    readers = models.ManyToManyField(User, through='UserBookRelation', related_name='books_read')
+    readers = models.ManyToManyField(User, through='UserBookRelation', related_name='books_reading')
+    rating = models.DecimalField(max_digits=3, decimal_places=2, default=None, null=True)
 
     def __str__(self):
         return f'Id {self.id}: {self.name}'
@@ -22,12 +23,17 @@ class UserBookRelation(models.Model):
         (4, 'Amazing'),
         (5, 'Unbeliveble')
     )
-
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='book_relations')
-    like = models.BooleanField(default=False)
-    in_bookmarks = models.BooleanField(default=False)
     rate = models.PositiveSmallIntegerField(choices=RATE_CHOICES, null=True)
 
     def __str__(self) -> str:
         return f'{self.user.username}: {self.book}, {self.rate}'
+
+    def save(self, *args, **kwargs):
+        from store.tests.logic import set_rating
+        creating = not self.pk
+        old_rating = self.rate
+        super().save(*args, **kwargs)
+        new_rating = self.rate
+        if old_rating != new_rating or creating:
+            set_rating(self.book)
+
--- store/tests/test_logic.py
+++ store/tests/test_logic.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
-from store.tests.logic import operations
+from store.tests.logic import operations, set_rating
+from store.models import User, Book
+from store.serializers import BookSerializers
 
 
 class LogicTestCase(TestCase):
@@ -14,3 +16,21 @@ class LogicTestCase(TestCase):
     def test_multiply(self):
         result = operations(6, 13, '*')
         self.assertEqual(78, result)
+
+
+class SetRatingTestCase(TestCase):
+    def setUp(self):
+        user_1 = User.objects.create(username='user1')
+        user_2 = User.objects.create(username='user2')
+
+        self.book_1 = Book.objects.create(name='Test book 1', price=25,
+                                     author_name='Author 1', owner=user_1)
+        book_2 = Book.objects.create(name='Test book 2', price=55,
+                                     author_name='Author 2', owner=user_2)
+        data = BookSerializers([self.book_1, book_2], many=True).data
+
+    def test_ok(self):
+        set_rating(self.book_1)
+        self.book_1.refresh_from_db()
+        self.assertEqual('4.67', str(self.book_1.rating))
+
